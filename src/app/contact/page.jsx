c8ba626e4cade320581e435789af8e717c169985
justice673 <fongejustice918@gmail.com>
2024-12-24 01:06:52 +0100

    contact page adjusted

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -183,14 +183,14 @@ export default function Contact() {
               <div className="h-64 bg-gray-200 ">
                 {/* Add your map component here */}
                 <div className="w-full h-full bg-red-300 flex items-center justify-center text-gray-500">
-                  <iframe
+                  {/* <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.0361667778!2d-74.30934281004731!3d40.69753994660521!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2scm!4v1734619517831!5m2!1sen!2scm"
                     style={{ border: 0, width: "100%", height: "100%" }}
                     alt="google map"
                     allowFullScreen=""
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
-                  ></iframe>
+                  ></iframe> */}
                 </div>
               </div>
             </div>
